749060cdac86bd357c6223c2d3893784f7217f59
Brian Shanken <bcshanken@gmail.com>
2020-10-06 19:33:18 -0400

    local memory storage added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  console.log("i have a script");
-  console.log(moment().format("h"));
-  console.log(moment().format("a"));
-  console.log(moment().format("MMMM Do YYYY"));
+  // console.log("i have a script");
+  // console.log(moment().format("h"));
+  // console.log(moment().format("a"));
+  // console.log(moment().format("MMMM Do YYYY"));
 
   
   // Dom Variables
@@ -16,16 +16,40 @@ $(document).ready(function () {
   function currentHourPlus() {
     if (moment().format("a") === "pm") {
       currentHour = parseInt(moment().format("h")) + 12;
-      console.log("works: "+currentHour);
+      // console.log("works: "+currentHour);
 
     } else {
       currentHour = moment().format("h");
     }
   }
+
+  function getPlans(){
+    for (i = 0; i < 9; i++) {
+      time = "";
+      hour = "";
+      if (9 + i > 12) {
+        var hour = 9 + i - 12;
+        time = hour + " PM";
+        if(localStorage.getItem(time)==null){
+          localStorage.setItem(time, "");
+          console.log(localStorage);
+          console.log(time);
+    }
+      } else {
+        var hour = 9 + i;
+        time = hour + " AM";
+        if(localStorage.getItem(time)==null){
+          localStorage.setItem(time, "");
+          console.log(localStorage);
+          console.log(time);
+    }
+      }
+  }    }
  
 
   // Function Calls
-  currentHourPlus()
+  currentHourPlus();
+  getPlans();
 
   for (i = 0; i < 9; i++) {
     time = "";
@@ -45,11 +69,11 @@ $(document).ready(function () {
     } else {
         timeClass="past"
     }
-    console.log(time);
-    console.log(currentHour);
+    // console.log(time);
+    // console.log(currentHour);
     var content = '<form class="row">'
     content += '<div class="col-md-1 hour">' + time + '</div>';
-    content += '<div class="col-md-10 description p-0"><textarea  class="'+ timeClass +'" id="'+ time +'">'+ hour +'</textarea></div>';
+    content += '<div class="col-md-10 description p-0"><textarea  class="'+ timeClass +'" id="'+ time +'">'+ localStorage.getItem(time) +'</textarea></div>';
     content += '<button class="col-md-1 saveBtn"><i class="far fa-save fa-lg"></i></button>';
     content += "</form>"
     $('.container').append(content);
@@ -57,13 +81,16 @@ $(document).ready(function () {
 
   // Event Listeners
 
-  console.log("The current hour is: " + currentHour);
+  // console.log("The current hour is: " + currentHour);
   
   $(".saveBtn").on("click", function () {
+    event.preventDefault()
     console.log("you clicked a button");
-    var time = $(this).parent().attr("id");
-    var text = $(this).siblings("textarea").val();
-    localStorage.setItem(time, text);
+    var task = $(this).parent().find("textarea").val();
+      var hour = $(this).parent().find("textarea").attr("id");
+      console.log("task:", task, hour);
+      localStorage.setItem(hour,task);
+
   });
 
 });
